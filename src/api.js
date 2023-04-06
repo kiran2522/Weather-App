99b439bade761234417d7b4e7dc68428f16c4b20
kiran2522 <20bd1a05a1@gmail.com>
2023-04-06 23:06:59 +0530

    first change

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c6e342a8d2msh12c73489cf631a2p1f7fd1jsndfd59ea54225',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f0f084ebd3cd39c9fbb74235a94edd07";
